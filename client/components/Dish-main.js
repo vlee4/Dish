b3c9f8f4e8f9fa8d3eefd745415f00cf9e7d4308
Victoria <vicglee1234007@gmail.com>
2020-05-01 17:56:20 -0500

    Predictions set to state, trying to stay D.R.Y w/ doPredict

--- client/components/Dish-main.js
+++ client/components/Dish-main.js
@@ -7,13 +7,15 @@ export class Dish extends React.Component {
     super()
     this.state = {
       selectImage: '',
-      // predictions: []
-      source: ''
+      source: '',
+      predictions: []
     }
     this.predictImage = this.predictImage.bind(this)
     // this.isValidFile = this.isValidFile.bind(this)
     this.getUpload = this.getUpload.bind(this)
     this.getSource = this.getSource.bind(this)
+    this.setPredictions = this.setPredictions.bind(this)
+    this.doPredict = this.doPredict.bind(this)
   }
 
   getUpload(event) {
@@ -26,6 +28,9 @@ export class Dish extends React.Component {
     this.setState({source: event.target.value})
   }
 
+  setPredictions(pdts) {
+    this.setState({predictions: pdts})
+  }
   // isValidFile() {
   //   console.log('What file was uploaded?', this.state.selectImage)
   //   let file = this.state.selectImage
@@ -36,6 +41,23 @@ export class Dish extends React.Component {
   //   }
   //   //will call predict Image if file is valid
   // }
+
+  doPredict(Clarifai, app, thisDish, imageInput) {
+    app.models
+      .predict(Clarifai.FOOD_MODEL, imageInput)
+      .then(function(resp) {
+        thisDish.setPredictions(resp.outputs[0].data.concepts)
+      })
+      .then(function() {
+        console.log('Here are my predictions', thisDish.state.predictions)
+      })
+      .catch(err =>
+        console.log(
+          "HOUSTON, WE'VE GOT A PROBLEM w/ the local IMAGE PREDICT",
+          err
+        )
+      )
+  }
 
   predictImage(event) {
     //REF: Clarifai Starter
@@ -49,13 +71,14 @@ export class Dish extends React.Component {
     const app = new Clarifai.App({
       apiKey: MY_API_KEY
     })
-
+    //IF THE FILE IS FROM LOCAL MACHINE
     if (this.state.source === 'file') {
       const file = document.getElementById('filename').files[0]
       var reader = new FileReader()
       // const file = document.getElementById('filename').value
       console.log('My file', file)
-
+      console.log('Which This', this)
+      const thisDish = this
       //Supposedly converting file w/ some base64 stuffs
       //Load is fired when a read has completed successfully
       reader.addEventListener(
@@ -64,16 +87,21 @@ export class Dish extends React.Component {
           var localBase64 = reader.result.split('base64,')[1] //the FileReader result = the file's contents, which is only valid after the read is complete
           console.log('PREDICTING')
           //Identify image from image data: just console.logging right now
-          app.models
-            .predict(Clarifai.FOOD_MODEL, {base64: localBase64})
-            .then(resp => console.log('MY RESPONSE', resp))
-            // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
-            .catch(err =>
-              console.log(
-                "HOUSTON, WE'VE GOT A PROBLEM w/ the local IMAGE PREDICT",
-                err
-              )
-            )
+          thisDish.doPredict(Clarifai, app, thisDish, {base64: localBase64})
+          // app.models
+          //   .predict(Clarifai.FOOD_MODEL, {base64: localBase64})
+          //   .then(function (resp) {
+          //     thisDish.setPredictions(resp.outputs[0].data.concepts)
+          //   })
+          //   .then(function () {
+          //     console.log('Here are my predictions', thisDish.state.predictions)
+          //   })
+          //   .catch((err) =>
+          //     console.log(
+          //       "HOUSTON, WE'VE GOT A PROBLEM w/ the local IMAGE PREDICT",
+          //       err
+          //     )
+          //   )
         },
         false
       )
@@ -81,17 +109,20 @@ export class Dish extends React.Component {
         reader.readAsDataURL(file)
       }
     } else if (this.state.source === 'url') {
+      //IF THE IMAGE FROM ONLINE
       console.log('URL button was clicked')
       const img = document.getElementById('imageUrl').value
       console.log('img', img)
 
-      app.models
-        .predict(Clarifai.FOOD_MODEL, {url: img})
-        .then(resp => console.log('MY RESPONSE', resp))
-        // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
-        .catch(err =>
-          console.log('An ERROR HATH OCCURRED w/ the IMAGE URL PREDICT', err)
-        )
+      this.doPredict(Clarifai, app, this, {url: img})
+
+      // app.models
+      //   .predict(Clarifai.FOOD_MODEL, {url: img})
+      //   .then((resp) => console.log('MY RESPONSE', resp))
+      //   // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
+      //   .catch((err) =>
+      //     console.log('An ERROR HATH OCCURRED w/ the IMAGE URL PREDICT', err)
+      //   )
     }
   }
 
