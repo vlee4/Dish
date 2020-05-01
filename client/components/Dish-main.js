876a6d78837afae97af5d1f14fd851ac1a2e5b47
Victoria <vicglee1234007@gmail.com>
2020-05-01 15:58:38 -0500

    Image Url prediction works

--- client/components/Dish-main.js
+++ client/components/Dish-main.js
@@ -7,16 +7,23 @@ export class Dish extends React.Component {
     super()
     this.state = {
       selectImage: '',
-      predictions: []
+      // predictions: []
+      source: ''
     }
     this.predictImage = this.predictImage.bind(this)
     // this.isValidFile = this.isValidFile.bind(this)
     this.getUpload = this.getUpload.bind(this)
+    this.getSource = this.getSource.bind(this)
   }
 
   getUpload(event) {
     this.setState({selectImage: event.target.value})
     console.log('Is this the image?', event.target.value)
+  }
+  //use source later to determine where photo came from (ie. url or upload)
+  getSource(event) {
+    console.log('SOURCE', event.target.value)
+    this.setState({source: event.target.value})
   }
 
   // isValidFile() {
@@ -30,15 +37,11 @@ export class Dish extends React.Component {
   //   //will call predict Image if file is valid
   // }
 
-  predictImage(event, source) {
-    //use source later to determine where photo came from (ie. url or upload)
+  predictImage(event) {
+    //REF: Clarifai Starter
     event.preventDefault()
     console.log('EVENT', event)
-    console.log('value', event.target.value)
-    const file = document.getElementById('filename').files[0]
-    var reader = new FileReader()
-    // const file = document.getElementById('filename').value
-    console.log('My file', file)
+    // console.log('value', event.target.value)
 
     //Make Clarifai app instance
     const Clarifai = require('clarifai')
@@ -47,24 +50,48 @@ export class Dish extends React.Component {
       apiKey: MY_API_KEY
     })
 
-    //Supposedly converting file w/ some base64 stuffs
-    //Load is fired when a read has completed successfully
-    reader.addEventListener(
-      'load',
-      function() {
-        var localBase64 = reader.result.split('base64,')[1] //the FileReader result = the file's contents, which is only valid after the read is complete
-        console.log('PREDICTING')
-        //Identify image from image data: just console.logging right now
-        app.models
-          .predict(Clarifai.FOOD_MODEL, {base64: localBase64})
-          .then(resp => console.log('MY RESPONSE', resp))
-          // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
-          .catch(err => console.log(err))
-      },
-      false
-    )
-    if (file) {
-      reader.readAsDataURL(file)
+    if (this.state.source === 'file') {
+      const file = document.getElementById('filename').files[0]
+      var reader = new FileReader()
+      // const file = document.getElementById('filename').value
+      console.log('My file', file)
+
+      //Supposedly converting file w/ some base64 stuffs
+      //Load is fired when a read has completed successfully
+      reader.addEventListener(
+        'load',
+        function() {
+          var localBase64 = reader.result.split('base64,')[1] //the FileReader result = the file's contents, which is only valid after the read is complete
+          console.log('PREDICTING')
+          //Identify image from image data: just console.logging right now
+          app.models
+            .predict(Clarifai.FOOD_MODEL, {base64: localBase64})
+            .then(resp => console.log('MY RESPONSE', resp))
+            // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
+            .catch(err =>
+              console.log(
+                "HOUSTON, WE'VE GOT A PROBLEM w/ the local IMAGE PREDICT",
+                err
+              )
+            )
+        },
+        false
+      )
+      if (file) {
+        reader.readAsDataURL(file)
+      }
+    } else if (this.state.source === 'url') {
+      console.log('URL button was clicked')
+      const img = document.getElementById('imageUrl').value
+      console.log('img', img)
+
+      app.models
+        .predict(Clarifai.FOOD_MODEL, {url: img})
+        .then(resp => console.log('MY RESPONSE', resp))
+        // .then((resp) => console.log(resp.outputs[0].data.concepts[0].name))
+        .catch(err =>
+          console.log('An ERROR HATH OCCURRED w/ the IMAGE URL PREDICT', err)
+        )
     }
   }
 
@@ -76,7 +103,12 @@ export class Dish extends React.Component {
         <form onSubmit={this.predictImage}>
           <div>
             <label htmlFor="filename">Upload an image</label>
-            <button type="submit" name="filename">
+            <button
+              type="submit"
+              name="filename"
+              onClick={this.getSource}
+              value="file"
+            >
               Predict
             </button>
             {/* input could also be a captured image using the computer's cam, use input type=capture */}
@@ -87,9 +119,27 @@ export class Dish extends React.Component {
               placeholder="Filename"
               accept="image/*"
               size="80"
-              value={this.state.selectImage}
+              // value={this.state.selectImage}
               onChange={this.getUpload}
             />
+          </div>
+          <div>
+            <label htmlFor="imageUrl">Predict via image url</label>
+            <input
+              type="text"
+              id="imageUrl"
+              name="imageUrl"
+              placeholder="Image URL"
+              size="80"
+            />
+            <button
+              type="submit"
+              name="imageUrl"
+              value="url"
+              onClick={this.getSource}
+            >
+              Predict
+            </button>
           </div>
         </form>
       </div>
@@ -98,3 +148,5 @@ export class Dish extends React.Component {
 }
 
 export default connect()(Dish)
+
+//REF: Clarifai Starter: https://github.com/Clarifai/javascript-starter
